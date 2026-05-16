a15b2db0687f8fe5926e072c6903aee7794a8050
pyy156941 <pyy156941@126.com>
2026-05-17 01:23:31 +0800

    Add CF2228 contest code

---
+++ contest/CF2228/C1.cpp
@@ -0,0 +1,282 @@
+// Problem: C1. Cirno and Number (Easy Version)
+// Contest: Codeforces - Codeforces Round 1098 (Div. 2)
+// URL: https://codeforces.com/contest/2228/problem/C1
+// Memory Limit: 256 MB
+// Time Limit: 2000 ms
+// 
+// Powered by CP Editor (https://cpeditor.org)
+
+#include <bits/stdc++.h>
+#pragma GCC optimize("O3,Ofast,unroll-loops")
+#pragma GCC target("avx2,bmi,bmi2,popcnt,lzcnt")
+
+using namespace std;
+
+#define pb push_back
+#define eb emplace_back
+#define ls(x) x << 1
+#define rs(x) x << 1 | 1
+#define lowbit(x) (x & (-x))
+#define ctz(x) (__builtin_ctz(x))
+#define ppc(x) (__builtin_popcount(x))
+
+using ll = long long;
+using ull = unsigned long long;
+using ui = unsigned int;
+using i128 = __int128;
+
+#define gc() getchar()
+#define pc(x) putchar(x)
+#define isdigit(x) (x >= '0' && x <= '9')
+#define least2p(x) ((x == 1) ? 0 : __lg(x) + ((x & (x - 1)) != 0))
+#define debug(x) cerr << #x << " : " << x << endl;
+
+#define yn(x) \
+do \
+{ \
+    cout << (x ? 'Y' : 'N'); \
+    cout << (x ? 'e' : 'o'); \
+    cout << (x ? 's' : '\n'); \
+    if (!x) cout << '\n'; \
+} while(0)
+
+#define ync(x) \
+do \
+{ \
+    cout << (x ? 'Y' : 'N'); \
+    cout << (x ? 'E' : 'O'); \
+    cout << (x ? 'S' : '\n'); \
+    if (!x) cout << '\n'; \
+} while(0)
+
+#define ynl(x) \
+do \
+{ \
+    cout << (x ? 'y' : 'n'); \
+    cout << (x ? 'e' : 'o'); \
+    cout << (x ? 's' : '\n'); \
+    if (!x) cout << '\n'; \
+} while(0)
+
+istream& operator >> (istream& cin, i128& x)
+{
+    x = 0;
+    int f = 1;
+    char ch;
+    ch = cin.get();
+    while (ch == ' ' || ch == '\n' || ch == '\t') ch = cin.get();
+    if (ch == '-')
+    {
+        f = -1;
+        ch = cin.get();
+    }
+    while (isdigit(ch))
+    {
+        x = x * 10 + (ch - '0');
+        ch = cin.get();
+    }
+    cin.putback(ch);
+    x *= f;
+    return cin;
+}
+
+ostream& operator << (ostream& cout, i128 x)
+{
+    if (x == 0)
+    {
+        cout << '0';
+        return cout;
+    }
+    if (x < 0)
+    {
+        cout << '-';
+        x = -x;
+    }
+    if (x >= 10) cout << (x / 10);
+    cout << (char)('0' + (x % 10));
+    return cout;
+}
+
+template <typename ... Args>
+void multi_read(Args& ... args)
+{
+    ((cin >> args), ...);
+}
+
+template <typename ... Args>
+void multi_write(Args ... args)
+{
+    ((cout << args << " "), ...);
+}
+
+template <typename ... Args>
+void multi_write_endl(Args ... args)
+{
+    ((cout << args << " "), ...);
+    cout << endl;
+}
+
+template <typename T>
+T fastgcd(T a, T b) // unsigned only, requires C++20
+{
+	if (a < b) 
+	{
+		T temp = a;
+		a = b;
+		b = temp;
+	}
+	if (!b) return a;
+	a %= b;
+	if (!a) return b;
+	auto za = ctz(a);
+	auto zb = ctz(b);
+	a >>= za;
+	b >>= zb;
+	do 
+	{
+		T dif = a - b;
+		if (a > b) a = b, b = dif;
+		else b = b - a;
+		b >>= ctz(dif);
+	} while (!b);
+	return a << min(za, zb);
+}
+
+template <typename T>
+void exgcd(T a, T b, T &x, T &y)
+{
+	if (b == 0)
+	{
+		x = 1, y = 0;
+		return;
+	}
+	exgcd(b, a % b, y, x);
+	y -= a / b * x;
+}
+
+template <typename T>
+T mod_inv(T a, T p)
+{
+	T x, y;
+	exgcd(a, p, x, y);
+	return (x + p) % p;
+}
+
+template <typename T>
+T qpow(T a, T b, T mod)
+{
+	T ans = 1;
+	while (b)
+	{
+		if (b & 1) ans = ans * a % mod;
+		a = a * a % mod;
+		b >>= 1;
+	}
+	return ans;
+}
+
+bool multi_test = true;
+
+ll p10[18];
+
+ll as(int d, int l)
+{
+	ll res = 0;
+	for (int i = 1; i <= l; i++) res *= 10ll, res += d;
+	return res;
+}
+
+void solve()
+{
+	p10[0] = 1;
+	for (int i = 1; i < 18; i++) p10[i] = p10[i - 1] * 10ll;
+	ll a;
+	int n;
+	ll d1, d2;
+	cin >> a >> n >> d1 >> d2;
+	int d = 0;
+	ll tmp = a;
+	vector <int> da;
+	while (tmp)
+	{
+		da.pb(tmp % 10);
+		tmp /= 10;
+		d++;
+	}
+	reverse(da.begin(), da.end());
+	ll ans = 1e18;
+	ll up = 0;
+	if (!d1) up += d2;
+	else up += d1;
+	for (int i = 1; i <= d; i++) up *= 10ll, up += d1;
+	ans = min(ans, up - a);
+	if (!d1) ans = min(ans, a);
+	if (d > 1)
+	{
+		ll low = as(d2, d - 1);
+		ans = min(ans, a - low);
+	}
+	// cerr << ans << ' ' << up << ' ' << low << endl;
+	ll cs = 0;
+	for (int i = 0; i < d; i++)
+	{
+		int cur = da[i];
+		int rest = d - i - 1;
+		if (!i && !d1)
+		{
+			if (d2 == cur)
+			{
+				cs *= 10ll;
+				cs += d2;
+				ans = min(ans, abs(cs * p10[rest] + as(d1, rest) - a));
+				ans = min(ans, abs(cs * p10[rest] + as(d2, rest) - a));
+			}
+			else
+			{
+				if (d2 > cur) ans = min(ans, d2 * p10[rest] + as(d1, rest) - a);
+				else ans = min(ans, a - d2 * p10[rest] - as(d2, rest));
+				break;
+			}
+		}
+		else
+		{
+			if (d1 == cur)
+			{
+				cs *= 10ll;
+				cs += d1;
+				ans = min(ans, abs(cs * p10[rest] + as(d1, rest) - a));
+				ans = min(ans, abs(cs * p10[rest] + as(d2, rest) - a));
+				ans = min(ans, (cs - d1 + d2) * p10[rest] + as(d1, rest) - a);
+			}
+			else if (d2 == cur)
+			{
+				cs *= 10ll;
+				cs += d2;
+				ans = min(ans, abs(cs * p10[rest] + as(d1, rest) - a));
+				ans = min(ans, abs(cs * p10[rest] + as(d2, rest) - a));
+				ans = min(ans, a - (cs - d2 + d1) * p10[rest] - as(d2, rest));
+			}
+			else
+			{
+				cs *= 10ll;
+				if (d1 > cur) ans = min(ans, (cs + d1) * p10[rest] + as(d1, rest) - a);
+				else ans = min(ans, a - (cs + d1) * p10[rest] - as(d2, rest));
+				if (d2 > cur) ans = min(ans, (cs + d2) * p10[rest] + as(d1, rest) - a);
+				else ans = min(ans, a - (cs + d2) * p10[rest] - as(d2, rest));
+				break;
+			}
+		}
+	}
+	cout << ans << endl;
+	return;
+}
+
+int main()
+{
+	ios :: sync_with_stdio(false);
+	cin.tie(nullptr);
+	int _ = 1;
+	if (multi_test) cin >> _;
+	while (_--) solve();
+	return 0;
+}
---
+++ contest/CF2228/E1.cpp
@@ -0,0 +1,236 @@
+// Problem: E1. Amanojaku and Sequence (Easy Version)
+// Contest: Codeforces - Codeforces Round 1098 (Div. 2)
+// URL: https://codeforces.com/contest/2228/problem/E1
+// Memory Limit: 512 MB
+// Time Limit: 4000 ms
+// 
+// Powered by CP Editor (https://cpeditor.org)
+
+#include <bits/stdc++.h>
+#pragma GCC optimize("O3,Ofast,unroll-loops")
+#pragma GCC target("avx2,bmi,bmi2,popcnt,lzcnt")
+
+using namespace std;
+
+#define pb push_back
+#define eb emplace_back
+#define ls(x) x << 1
+#define rs(x) x << 1 | 1
+#define lowbit(x) (x & (-x))
+#define ctz(x) (__builtin_ctz(x))
+#define ppc(x) (__builtin_popcount(x))
+
+using ll = long long;
+using ull = unsigned long long;
+using ui = unsigned int;
+using i128 = __int128;
+
+#define gc() getchar()
+#define pc(x) putchar(x)
+#define isdigit(x) (x >= '0' && x <= '9')
+#define least2p(x) ((x == 1) ? 0 : __lg(x) + ((x & (x - 1)) != 0))
+#define debug(x) cerr << #x << " : " << x << endl;
+
+#define yn(x) \
+do \
+{ \
+    cout << (x ? 'Y' : 'N'); \
+    cout << (x ? 'e' : 'o'); \
+    cout << (x ? 's' : '\n'); \
+    if (!x) cout << '\n'; \
+} while(0)
+
+#define ync(x) \
+do \
+{ \
+    cout << (x ? 'Y' : 'N'); \
+    cout << (x ? 'E' : 'O'); \
+    cout << (x ? 'S' : '\n'); \
+    if (!x) cout << '\n'; \
+} while(0)
+
+#define ynl(x) \
+do \
+{ \
+    cout << (x ? 'y' : 'n'); \
+    cout << (x ? 'e' : 'o'); \
+    cout << (x ? 's' : '\n'); \
+    if (!x) cout << '\n'; \
+} while(0)
+
+istream& operator >> (istream& cin, i128& x)
+{
+    x = 0;
+    int f = 1;
+    char ch;
+    ch = cin.get();
+    while (ch == ' ' || ch == '\n' || ch == '\t') ch = cin.get();
+    if (ch == '-')
+    {
+        f = -1;
+        ch = cin.get();
+    }
+    while (isdigit(ch))
+    {
+        x = x * 10 + (ch - '0');
+        ch = cin.get();
+    }
+    cin.putback(ch);
+    x *= f;
+    return cin;
+}
+
+ostream& operator << (ostream& cout, i128 x)
+{
+    if (x == 0)
+    {
+        cout << '0';
+        return cout;
+    }
+    if (x < 0)
+    {
+        cout << '-';
+        x = -x;
+    }
+    if (x >= 10) cout << (x / 10);
+    cout << (char)('0' + (x % 10));
+    return cout;
+}
+
+template <typename ... Args>
+void multi_read(Args& ... args)
+{
+    ((cin >> args), ...);
+}
+
+template <typename ... Args>
+void multi_write(Args ... args)
+{
+    ((cout << args << " "), ...);
+}
+
+template <typename ... Args>
+void multi_write_endl(Args ... args)
+{
+    ((cout << args << " "), ...);
+    cout << endl;
+}
+
+template <typename T>
+T fastgcd(T a, T b) // unsigned only, requires C++20
+{
+	if (a < b) 
+	{
+		T temp = a;
+		a = b;
+		b = temp;
+	}
+	if (!b) return a;
+	a %= b;
+	if (!a) return b;
+	auto za = ctz(a);
+	auto zb = ctz(b);
+	a >>= za;
+	b >>= zb;
+	do 
+	{
+		T dif = a - b;
+		if (a > b) a = b, b = dif;
+		else b = b - a;
+		b >>= ctz(dif);
+	} while (!b);
+	return a << min(za, zb);
+}
+
+template <typename T>
+void exgcd(T a, T b, T &x, T &y)
+{
+	if (b == 0)
+	{
+		x = 1, y = 0;
+		return;
+	}
+	exgcd(b, a % b, y, x);
+	y -= a / b * x;
+}
+
+template <typename T>
+T mod_inv(T a, T p)
+{
+	T x, y;
+	exgcd(a, p, x, y);
+	return (x + p) % p;
+}
+
+template <typename T>
+T qpow(T a, T b, T mod)
+{
+	T ans = 1;
+	while (b)
+	{
+		if (b & 1) ans = ans * a % mod;
+		a = a * a % mod;
+		b >>= 1;
+	}
+	return ans;
+}
+
+bool multi_test = true;
+
+constexpr ll mod = 998244353;
+
+int v[300001];
+ll fact[300001], invf[300001];
+
+ll binom(ll n, ll m)
+{
+	if (m > n) return 0;
+	return fact[n] * invf[m] % mod * invf[n - m] % mod;
+}
+
+void solve()
+{
+	int n, q;
+	cin >> n >> q;
+	fact[0] = fact[1] = invf[1] = invf[0] = 1;
+	for (int i = 2; i <= n; i++) fact[i] = (fact[i - 1] * (ll)i) % mod;
+	for (int i = 1; i <= n; i++) cin >> v[i];
+	int mode, l, r, m;
+	cin >> mode >> l >> r >> m;
+	ll s = 0;
+	int p = 0;
+	int las = r + 1;
+	for (int i = l; i <= r; i++) 
+	{
+		if (v[i] != -1) s += (ll)v[i];
+		else p++, las = i;
+	}
+	if (s > m) 
+	{
+		cout << 0 << endl;
+		return;
+	} 
+	s = m - s;
+	int a = 0, c = 0;
+	ll ans = 0;
+	for (int i = l; i < las; i++)
+	{
+		if (v[i] == -1) a++;
+		else c += v[i];
+		int b = p - a;
+		
+	}
+	ans = (ans + m * m % mod * binom(s + p - 1, p - 1) % mod) % mod;
+	cout << ans << endl;
+	return;
+}
+
+int main()
+{
+	ios :: sync_with_stdio(false);
+	cin.tie(nullptr);
+	int _ = 1;
+	if (multi_test) cin >> _;
+	while (_--) solve();
+	return 0;
+}
