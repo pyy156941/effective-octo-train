14a81e006407f5d94c0f86cc178f728e3aec00f2
pyy156941 <pyy156941@126.com>
2026-06-03 15:39:50 +0800

    Add CF2146E

---
+++ contest/CF2146VP/E.cpp
@@ -0,0 +1,260 @@
+// Problem: E. Yet Another MEX Problem
+// Contest: Codeforces - Codeforces Round 1052 (Div. 2)
+// URL: https://codeforces.com/contest/2146/problem/E
+// Memory Limit: 512 MB
+// Time Limit: 2000 ms
+// 
+// Powered by CP Editor (https://cpeditor.org)
+
+#include <bits/stdc++.h>
+#pragma GCC optimize("O3,Ofast,unroll-loops")
+#pragma GCC target("avx2,bmi,bmi2,popcnt,lzcnt")
+
+using namespace std;
+
+#define pb push_back
+#define eb emplace_back
+#define ls(x) x << 1
+#define rs(x) x << 1 | 1
+#define lowbit(x) (x & (-x))
+#define ctz(x) (__builtin_ctz(x))
+#define ppc(x) (__builtin_popcount(x))
+
+using ll = long long;
+using ull = unsigned long long;
+using ui = unsigned int;
+using i128 = __int128;
+
+#define gc() getchar()
+#define pc(x) putchar(x)
+#define isdigit(x) (x >= '0' && x <= '9')
+#define least2p(x) ((x == 1) ? 0 : __lg(x) + ((x & (x - 1)) != 0))
+#define debug(x) cerr << #x << " : " << x << endl;
+
+#define yn(x) \
+do \
+{ \
+    cout << (x ? 'Y' : 'N'); \
+    cout << (x ? 'e' : 'o'); \
+    cout << (x ? 's' : '\n'); \
+    if (!x) cout << '\n'; \
+} while(0)
+
+#define ync(x) \
+do \
+{ \
+    cout << (x ? 'Y' : 'N'); \
+    cout << (x ? 'E' : 'O'); \
+    cout << (x ? 'S' : '\n'); \
+    if (!x) cout << '\n'; \
+} while(0)
+
+#define ynl(x) \
+do \
+{ \
+    cout << (x ? 'y' : 'n'); \
+    cout << (x ? 'e' : 'o'); \
+    cout << (x ? 's' : '\n'); \
+    if (!x) cout << '\n'; \
+} while(0)
+
+istream& operator >> (istream& cin, i128& x)
+{
+    x = 0;
+    int f = 1;
+    char ch;
+    ch = cin.get();
+    while (ch == ' ' || ch == '\n' || ch == '\t') ch = cin.get();
+    if (ch == '-')
+    {
+        f = -1;
+        ch = cin.get();
+    }
+    while (isdigit(ch))
+    {
+        x = x * 10 + (ch - '0');
+        ch = cin.get();
+    }
+    cin.putback(ch);
+    x *= f;
+    return cin;
+}
+
+ostream& operator << (ostream& cout, i128 x)
+{
+    if (x == 0)
+    {
+        cout << '0';
+        return cout;
+    }
+    if (x < 0)
+    {
+        cout << '-';
+        x = -x;
+    }
+    if (x >= 10) cout << (x / 10);
+    cout << (char)('0' + (x % 10));
+    return cout;
+}
+
+template <typename ... Args>
+void multi_read(Args& ... args)
+{
+    ((cin >> args), ...);
+}
+
+template <typename ... Args>
+void multi_write(Args ... args)
+{
+    ((cout << args << " "), ...);
+}
+
+template <typename ... Args>
+void multi_write_endl(Args ... args)
+{
+    ((cout << args << " "), ...);
+    cout << endl;
+}
+
+template <typename T>
+T fastgcd(T a, T b) // unsigned only, requires C++20
+{
+	if (a < b) 
+	{
+		T temp = a;
+		a = b;
+		b = temp;
+	}
+	if (!b) return a;
+	a %= b;
+	if (!a) return b;
+	auto za = ctz(a);
+	auto zb = ctz(b);
+	a >>= za;
+	b >>= zb;
+	do 
+	{
+		T dif = a - b;
+		if (a > b) a = b, b = dif;
+		else b = b - a;
+		b >>= ctz(dif);
+	} while (!b);
+	return a << min(za, zb);
+}
+
+template <typename T>
+void exgcd(T a, T b, T &x, T &y)
+{
+	if (b == 0)
+	{
+		x = 1, y = 0;
+		return;
+	}
+	exgcd(b, a % b, y, x);
+	y -= a / b * x;
+}
+
+template <typename T>
+T mod_inv(T a, T p)
+{
+	T x, y;
+	exgcd(a, p, x, y);
+	return (x + p) % p;
+}
+
+template <typename T>
+T qpow(T a, T b, T mod)
+{
+	T ans = 1;
+	while (b)
+	{
+		if (b & 1) ans = ans * a % mod;
+		a = a * a % mod;
+		b >>= 1;
+	}
+	return ans;
+}
+
+bool multi_test = true;
+
+int tree[1200001], tag[1200001];
+
+void pushup(int cur)
+{
+	tree[cur] = max(tree[ls(cur)], tree[rs(cur)]);
+}
+
+void build(int cur, int s, int t)
+{
+	tree[cur] = tag[cur] = 0;
+	if (s == t) return;
+	int mid = (s + t) >> 1;
+	build(ls(cur), s, mid);
+	build(rs(cur), mid + 1, t);
+}
+
+void pushdown(int cur)
+{
+	if (!tag[cur]) return;
+	tree[ls(cur)] += tag[cur];
+	tree[rs(cur)] += tag[cur];
+	tag[ls(cur)] += tag[cur];
+	tag[rs(cur)] += tag[cur];
+	tag[cur] = 0;
+}
+
+void update(int cur, int p, int s, int t, int x)
+{
+	if (s == t)
+	{
+		tree[cur] = x;
+		return;
+	}
+	pushdown(cur);
+	int mid = (s + t) >> 1;
+	if (p <= mid) update(ls(cur), p, s, mid, x);
+	if (p > mid) update(rs(cur), p, mid + 1, t, x);
+	pushup(cur);
+}
+
+void update(int cur, int l, int r, int s, int t, int x)
+{
+	if (l <= s && t <= r)
+	{
+		tree[cur] += x;
+		tag[cur] += x;
+		return;
+	}
+	pushdown(cur);
+	int mid = (s + t) >> 1;
+	if (l <= mid) update(ls(cur), l, r, s, mid, x);
+	if (r > mid) update(rs(cur), l, r, mid + 1, t, x);
+	pushup(cur);
+}
+
+int a[300001];
+void solve()
+{
+	int n;
+	cin >> n;
+	for (int i = 1; i <= n; i++) cin >> a[i];
+	build(1, 0, n);
+	for (int i = 1; i <= n; i++)
+	{
+		update(1, a[i], 0, n, 0);
+		if (a[i]) update(1, 0, a[i] - 1, 0, n, 1);
+		cout << tree[1] << ' ';
+	}
+	cout << endl;
+	return;
+}
+
+int main()
+{
+	ios :: sync_with_stdio(false);
+	cin.tie(nullptr);
+	int _ = 1;
+	if (multi_test) cin >> _;
+	while (_--) solve();
+	return 0;
+}
