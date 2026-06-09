05b9d553ac10c020ffcadd88ac5ddc055e94999e
pyy156941 <pyy156941@126.com>
2026-06-10 01:25:44 +0800

    Add CF2233 contest code

---
+++ contest/CF2233/D.cpp
@@ -0,0 +1,296 @@
+// Problem: D. Goods on the Shelf
+// Contest: Codeforces - Educational Codeforces Round 191 (Rated for Div. 2)
+// URL: https://codeforces.com/contest/2233/problem/D
+// Memory Limit: 512 MB
+// Time Limit: 2000 ms
+// 
+// Powered by CP Editor (https://cpeditor.org)
+
+#include <bits/stdc++.h>
+#pragma GCC optimize("O3,Ofast,unroll-loops")
+#pragma GCC target("avx2,bmi,bmi2,popcnt,lzcnt")
+
+using namespace std;
+
+#define pb push_back
+#define eb emplace_back
+#define ls(x) x << 1
+#define rs(x) x << 1 | 1
+#define lowbit(x) (x & (-x))
+#define ctz(x) (__builtin_ctz(x))
+#define ppc(x) (__builtin_popcount(x))
+
+using ll = long long;
+using ull = unsigned long long;
+using ui = unsigned int;
+using i128 = __int128;
+
+#define gc() getchar()
+#define pc(x) putchar(x)
+#define isdigit(x) (x >= '0' && x <= '9')
+#define least2p(x) ((x == 1) ? 0 : __lg(x) + ((x & (x - 1)) != 0))
+#define debug(x) cerr << #x << " : " << x << endl;
+
+#define yn(x) \
+do \
+{ \
+    cout << (x ? 'Y' : 'N'); \
+    cout << (x ? 'e' : 'o'); \
+    cout << (x ? 's' : '\n'); \
+    if (!x) cout << '\n'; \
+} while(0)
+
+#define ync(x) \
+do \
+{ \
+    cout << (x ? 'Y' : 'N'); \
+    cout << (x ? 'E' : 'O'); \
+    cout << (x ? 'S' : '\n'); \
+    if (!x) cout << '\n'; \
+} while(0)
+
+#define ynl(x) \
+do \
+{ \
+    cout << (x ? 'y' : 'n'); \
+    cout << (x ? 'e' : 'o'); \
+    cout << (x ? 's' : '\n'); \
+    if (!x) cout << '\n'; \
+} while(0)
+
+istream& operator >> (istream& cin, i128& x)
+{
+    x = 0;
+    int f = 1;
+    char ch;
+    ch = cin.get();
+    while (ch == ' ' || ch == '\n' || ch == '\t') ch = cin.get();
+    if (ch == '-')
+    {
+        f = -1;
+        ch = cin.get();
+    }
+    while (isdigit(ch))
+    {
+        x = x * 10 + (ch - '0');
+        ch = cin.get();
+    }
+    cin.putback(ch);
+    x *= f;
+    return cin;
+}
+
+ostream& operator << (ostream& cout, i128 x)
+{
+    if (x == 0)
+    {
+        cout << '0';
+        return cout;
+    }
+    if (x < 0)
+    {
+        cout << '-';
+        x = -x;
+    }
+    if (x >= 10) cout << (x / 10);
+    cout << (char)('0' + (x % 10));
+    return cout;
+}
+
+template <typename ... Args>
+void multi_read(Args& ... args)
+{
+    ((cin >> args), ...);
+}
+
+template <typename ... Args>
+void multi_write(Args ... args)
+{
+    ((cout << args << " "), ...);
+}
+
+template <typename ... Args>
+void multi_write_endl(Args ... args)
+{
+    ((cout << args << " "), ...);
+    cout << endl;
+}
+
+template <typename T>
+T fastgcd(T a, T b) // unsigned only, requires C++20
+{
+	if (a < b) 
+	{
+		T temp = a;
+		a = b;
+		b = temp;
+	}
+	if (!b) return a;
+	a %= b;
+	if (!a) return b;
+	auto za = ctz(a);
+	auto zb = ctz(b);
+	a >>= za;
+	b >>= zb;
+	do 
+	{
+		T dif = a - b;
+		if (a > b) a = b, b = dif;
+		else b = b - a;
+		b >>= ctz(dif);
+	} while (!b);
+	return a << min(za, zb);
+}
+
+template <typename T>
+void exgcd(T a, T b, T &x, T &y)
+{
+	if (b == 0)
+	{
+		x = 1, y = 0;
+		return;
+	}
+	exgcd(b, a % b, y, x);
+	y -= a / b * x;
+}
+
+template <typename T>
+T mod_inv(T a, T p)
+{
+	T x, y;
+	exgcd(a, p, x, y);
+	return (x + p) % p;
+}
+
+template <typename T>
+T qpow(T a, T b, T mod)
+{
+	T ans = 1;
+	while (b)
+	{
+		if (b & 1) ans = ans * a % mod;
+		a = a * a % mod;
+		b >>= 1;
+	}
+	return ans;
+}
+
+bool multi_test = true;
+
+int n;
+int a[200001], tmp[200001], cnt = 0;
+vector <int> pl[200001];
+
+int lst[200001];
+bool check(int u, int v)
+{
+	if (u < 1 || u > n || v < 1 || v > n) return false;
+	swap(a[u], a[v]);
+	bool ok = true;
+	for (int i = 1; i <= cnt; i++) lst[i] = 0;
+	for (int i = 1; i <= n; i++)
+	{
+		if (!lst[a[i]]) lst[a[i]] = i;
+		else 
+		{
+			if (lst[a[i]] != i - 1) 
+			{
+				ok = false;
+				break;
+			}
+			else lst[a[i]] = i;
+		}
+	}
+	swap(a[u], a[v]);
+	if (ok) return true;
+	else return false;
+}
+
+void solve()
+{
+	cin >> n;
+	cnt = 0;
+	for (int i = 1; i <= n; i++) pl[i].clear();
+	for (int i = 1; i <= n; i++)
+	{
+		cin >> a[i];
+		tmp[++cnt] = a[i];
+	}
+	sort(tmp + 1, tmp + cnt + 1);
+	cnt = unique(tmp + 1, tmp + cnt + 1) - tmp - 1;
+	for (int i = 1; i <= n; i++) 
+	{
+		a[i] = lower_bound(tmp + 1, tmp + cnt + 1, a[i]) - tmp;
+		pl[a[i]].pb(i);
+	}
+	for (int i = 1; i <= cnt; i++)
+	{
+		vector <pair <int, int>> rg;
+		vector <int> len;
+		int st = 0, las = 0;
+		for (auto it : pl[i])
+		{
+			if (!las) st = las = it;
+			else if (st == it - 1) st = it;
+			else 
+			{
+				rg.pb({las, st});
+				len.pb(st - las + 1);
+				st = las = it;
+			}
+		}
+		rg.pb({las, st});
+		len.pb(st - las + 1);
+		if (rg.size() == 1) continue;
+		if (rg.size() > 3) 
+		{
+			cout << "NO" << endl;
+			return;
+		}
+		if (rg.size() == 3)
+		{
+			if (len[0] > 1 && len[1] > 1 && len[2] > 1)
+			{
+				cout << "NO" << endl;
+				return;
+			}
+			bool ok = false;
+			if (len[0] == 1) ok |= check(rg[0].first, rg[1].second + 1);
+			if (len[2] == 1) ok |= check(rg[2].first, rg[1].first - 1);
+			if (ok) cout << "YES" << endl;
+			else cout << "NO" << endl;
+			return;
+		}
+		if (rg.size() == 2)
+		{
+			bool ok = false;
+			if (len[0] == 1)
+			{
+				ok |= check(rg[0].first, rg[1].second + 1);
+				ok |= check(rg[0].first, rg[1].first - 1);
+			}
+			if (len[1] == 1)
+			{
+				ok |= check(rg[1].first, rg[0].second + 1);
+				ok |= check(rg[1].first, rg[0].first - 1);
+			}	
+			ok |= check(rg[0].first, rg[0].second + 1);
+			ok |= check(rg[1].second, rg[1].first - 1);
+			if (ok) cout << "YES" << endl;
+			else cout << "NO" << endl;
+			return;
+		}
+	}
+	cout << "YES" << endl;
+	return;
+}
+
+int main()
+{
+	ios :: sync_with_stdio(false);
+	cin.tie(nullptr);
+	int _ = 1;
+	if (multi_test) cin >> _;
+	while (_--) solve();
+	return 0;
+}
