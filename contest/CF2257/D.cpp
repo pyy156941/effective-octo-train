8a096a04d39a528c3f513ff11c50bab8518eaa9c
pyy156941 <pyy156941@126.com>
2026-08-18 11:24:59 +0800

    Add CF2257 contest code

---
+++ contest/CF2257/D.cpp
@@ -0,0 +1,239 @@
+// Problem: D. Bermuda Rectangle
+// Contest: Codeforces - Codeforces Round 1117 (Div. 2)
+// URL: https://codeforces.com/contest/2257/problem/D
+// Memory Limit: 256 MB
+// Time Limit: 2000 ms
+// 
+// Powered by CP Editor (https://cpeditor.org)
+
+#include <bits/stdc++.h>
+#pragma GCC optimize("O3,Ofast,unroll-loops")
+#pragma GCC target("avx2,bmi,bmi2,popcnt,lzcnt")
+
+using namespace std;
+
+#define pb push_back
+#define eb emplace_back
+#define ls(x) x << 1
+#define rs(x) x << 1 | 1
+#define lowbit(x) (x & (-x))
+#define ctz(x) (__builtin_ctz(x))
+#define ppc(x) (__builtin_popcount(x))
+
+using ll = long long;
+using ull = unsigned long long;
+using ui = unsigned int;
+using i128 = __int128;
+
+#define gc() getchar()
+#define pc(x) putchar(x)
+#define isdigit(x) (x >= '0' && x <= '9')
+#define least2p(x) ((x == 1) ? 0 : __lg(x) + ((x & (x - 1)) != 0))
+#define debug(x) cerr << #x << " : " << x << endl;
+
+#define yn(x) \
+do \
+{ \
+    cout << (x ? 'Y' : 'N'); \
+    cout << (x ? 'e' : 'o'); \
+    cout << (x ? 's' : '\n'); \
+    if (!x) cout << '\n'; \
+} while(0)
+
+#define ync(x) \
+do \
+{ \
+    cout << (x ? 'Y' : 'N'); \
+    cout << (x ? 'E' : 'O'); \
+    cout << (x ? 'S' : '\n'); \
+    if (!x) cout << '\n'; \
+} while(0)
+
+#define ynl(x) \
+do \
+{ \
+    cout << (x ? 'y' : 'n'); \
+    cout << (x ? 'e' : 'o'); \
+    cout << (x ? 's' : '\n'); \
+    if (!x) cout << '\n'; \
+} while(0)
+
+istream& operator >> (istream& cin, i128& x)
+{
+    x = 0;
+    int f = 1;
+    char ch;
+    ch = cin.get();
+    while (ch == ' ' || ch == '\n' || ch == '\t') ch = cin.get();
+    if (ch == '-')
+    {
+        f = -1;
+        ch = cin.get();
+    }
+    while (isdigit(ch))
+    {
+        x = x * 10 + (ch - '0');
+        ch = cin.get();
+    }
+    cin.putback(ch);
+    x *= f;
+    return cin;
+}
+
+ostream& operator << (ostream& cout, i128 x)
+{
+    if (x == 0)
+    {
+        cout << '0';
+        return cout;
+    }
+    if (x < 0)
+    {
+        cout << '-';
+        x = -x;
+    }
+    if (x >= 10) cout << (x / 10);
+    cout << (char)('0' + (x % 10));
+    return cout;
+}
+
+template <typename ... Args>
+void multi_read(Args& ... args)
+{
+    ((cin >> args), ...);
+}
+
+template <typename ... Args>
+void multi_write(Args ... args)
+{
+    ((cout << args << " "), ...);
+}
+
+template <typename ... Args>
+void multi_write_endl(Args ... args)
+{
+    ((cout << args << " "), ...);
+    cout << endl;
+}
+
+template <typename T>
+T fastgcd(T a, T b) // unsigned only, requires C++20
+{
+	if (a < b) 
+	{
+		T temp = a;
+		a = b;
+		b = temp;
+	}
+	if (!b) return a;
+	a %= b;
+	if (!a) return b;
+	auto za = ctz(a);
+	auto zb = ctz(b);
+	a >>= za;
+	b >>= zb;
+	do 
+	{
+		T dif = a - b;
+		if (a > b) a = b, b = dif;
+		else b = b - a;
+		b >>= ctz(dif);
+	} while (!b);
+	return a << min(za, zb);
+}
+
+template <typename T>
+void exgcd(T a, T b, T &x, T &y)
+{
+	if (b == 0)
+	{
+		x = 1, y = 0;
+		return;
+	}
+	exgcd(b, a % b, y, x);
+	y -= a / b * x;
+}
+
+template <typename T>
+T mod_inv(T a, T p)
+{
+	T x, y;
+	exgcd(a, p, x, y);
+	return (x + p) % p;
+}
+
+template <typename T>
+T qpow(T a, T b, T mod)
+{
+	T ans = 1;
+	while (b)
+	{
+		if (b & 1) ans = ans * a % mod;
+		a = a * a % mod;
+		b >>= 1;
+	}
+	return ans;
+}
+
+bool multi_test = true;
+
+vector <ll> prep, accu, le, re, divs;
+void solve()
+{
+	ll s, x, y;
+	int q;
+	cin >> s >> q;
+	prep.clear();
+	accu.clear();
+	le.clear();
+	re.clear();
+	divs.clear();
+	for (ll i = 1; i * i <= s; i++)
+	{
+		if (s % i) continue;
+		divs.pb(i);
+		if (i * i != s) divs.pb(s / i);
+	}
+	sort(divs.begin(), divs.end());
+	reverse(divs.begin(), divs.end());
+	ll l = 1, r = 0, curs = 0;
+	for (auto d : divs)
+	{
+		r = s / d;
+		le.pb(l);
+		re.pb(r);
+		prep.pb(d);
+		curs += d * (r - l + 1);
+		accu.pb(curs);
+		// cerr << l << ' ' << r << ' ' << d << ' ' << curs << endl;
+		l = r + 1;
+	}
+	for (int i = 1; i <= q; i++)
+	{
+		cin >> x >> y;
+		int leblk = upper_bound(prep.begin(), prep.end(), x, greater <ll> ()) - prep.begin() - 1;
+		int reblk = upper_bound(le.begin(), le.end(), y) - le.begin() - 1;
+		ll clip = re[leblk];
+		ll ans = min(clip, y) * x;
+		if (clip >= y)
+		{
+			cout << ans << endl;
+			continue;
+		}
+		// cerr << leblk << ' ' << reblk << endl;
+		if (reblk > leblk + 1) ans += accu[reblk - 1] - accu[leblk];
+		ans += (y - le[reblk] + 1ll) * prep[reblk];
+		cout << ans << endl;
+	}
+	return;
+}
+
+int main()
+{
+	ios :: sync_with_stdio(false);
+	cin.tie(nullptr);
+	int _ = 1;
+	if (multi_test) cin >> _;
+	while (_--) solve();
+	return 0;
+}
